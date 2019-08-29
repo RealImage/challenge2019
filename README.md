469ebb6ffd059a237798d7c6339e5a542482ca12
Vinay <me@vinaykumar.co>
2019-08-29 16:48:06 +0530

    Create qube module and other changes

---
+++ README.md
@@ -0,0 +1,24 @@
+## RealImage challenge 2019
+[Problem statement Link](https://github.com/RealImage/challenge2019/blob/master/README.md)
+###Tech stack
+- golang v1.10.4
+
+###Execution
+- The extracted Qube folder should reside within go path.
+- `cd src/qube` and `./bin/main`
+<br> or <br>
+- `cd src/qube` and `go run main`
+
+###Folder structure
+- module based structure. `src` folder will contain modules.
+- main.go file resides in `module` root 
+- `module/static` contains input and output(generated automatically after running the code) csv.
+- `module/structs` contains structs
+- `module/prepocess` contains file which converts and stores 
+    the data read from input csv file in data structure.
+ 
+### Misc
+- Refer `main.go` for solution for both the problems.
+- For problem 2 optimal solution would be using `min-cost max-flow` algorithm. Since I do not have required knowledge 
+of the algorithm, have implement another logic which is explained in comments section in respective file.
+The applied logic is not optimal but should work for most of the use cases.
---
+++ src/qube/helpers/common.go
@@ -0,0 +1,97 @@
+package helpers
+
+import (
+	"fmt"
+	"qube/structs"
+	"bufio"
+	"container/heap"
+	"encoding/csv"
+	"log"
+	"os"
+	//"fmt"
+	"strconv"
+)
+
+func ParseContentsFromCsv(filename string) *csv.Reader{
+	csvFile, fileReadingErr := os.Open(filename)
+	if fileReadingErr != nil {
+		log.Fatalf("Unable to read file %s", filename)
+	}
+	r := csv.NewReader(bufio.NewReader(csvFile))
+	return r
+}
+
+func WriteDataToCsv(filename string, data [] structs.ResultFormatStruct) string{
+	path := "./static"
+	file, err := os.Create(fmt.Sprintf("%s/%s",path,filename))
+	if err != nil {
+		log.Fatal(fmt.Sprintf("Unable to create file %s", fmt.Sprintf("%s/%s",path,filename)))
+	}
+	defer file.Close()
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+	for _, value := range data {
+		temp := [] string{value.DeliveryId, strconv.FormatBool(value.DeliveryPossible), value.PartnerId, value.Cost}
+		err := writer.Write(temp)
+		if err != nil {
+			log.Fatalf("Cannot write to file %s/%s",path,filename)
+		}
+    }
+	return fmt.Sprintf("%s/%s",path,filename)
+}
+
+
+// Min priority heap
+type Item struct {
+	Value    string
+	Priority int
+	Index int
+}
+
+// A PriorityQueue implements heap.Interface and holds Items.
+type PriorityQueue []*Item
+
+func (pq PriorityQueue) Len() int { return len(pq) }
+
+func (pq PriorityQueue) Less(i, j int) bool {
+	return pq[i].Priority < pq[j].Priority
+}
+
+func (pq PriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].Index = i
+	pq[j].Index = j
+}
+
+func (pq *PriorityQueue) Push(x interface{}) {
+	n := len(*pq)
+	item := x.(*Item)
+	item.Index = n
+	*pq = append(*pq, item)
+}
+
+func (pq *PriorityQueue) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	item.Index = -1 // for safety
+	*pq = old[0 : n-1]
+	return item
+}
+
+// update modifies the priority and value of an Item in the queue.
+func (pq *PriorityQueue) update(item *Item, value string, priority int) {
+	item.Value = value
+	item.Priority = priority
+	heap.Fix(pq, item.Index)
+}
+
+
+func (pq *PriorityQueue) ToArray() [] Item{
+	var temp [] Item
+	for pq.Len() > 0 {
+		item := heap.Pop(pq).(*Item)
+		temp = append(temp, *item)
+	}
+	return temp
+}
