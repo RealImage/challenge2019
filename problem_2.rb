a83c7e27d363bbca0e8122786bba0ce7600629a4
Bhaskar <bkotu6717@gmail.com>
2019-12-09 20:29:56 +0530

    Read the input from input.csv and write the output for output2.csv

--- problem_2.rb
+++ problem_2.rb
@@ -1,5 +1,6 @@
 
 require 'pry'
+require 'csv'
 
 def partner_capacity
     {
@@ -40,22 +41,6 @@ def get_partners_list_by_theater(theater)
   }[theater.to_sym]
 end
 
-def get_todo_deliveries
-
-  # [
-  #   {delivery_id: 'D1', size_slab_in_gb: 100, theater: 'T1'},
-  #   {delivery_id: 'D2', size_slab_in_gb: 240, theater: 'T1'},
-  #   {delivery_id: 'D3', size_slab_in_gb: 260, theater: 'T1'},
-  # ]
-  
-  [
-    {delivery_id: 'D1', size_slab_in_gb: 150, theater: 'T1'},
-    {delivery_id: 'D2', size_slab_in_gb: 325, theater: 'T2'},
-    {delivery_id: 'D3', size_slab_in_gb: 510, theater: 'T1'},
-    {delivery_id: 'D4', size_slab_in_gb: 700, theater: 'T2'}
-  ]
-end
-
 def partner_cost_evaluation(partner, size_slab_in_gb)
   consumption_cost = partner[:cost_per_gb] * size_slab_in_gb
   [consumption_cost, partner[:minimum_cost]].max
@@ -84,29 +69,21 @@ end
 
 
 # Proecss the deliveries in descending order, so that we can make sure maximum deliveries are taken care
-def find_delivery_partners
-  todo_deliveries = get_todo_deliveries.sort {|x, y | y[:size_slab_in_gb] <=> x[:size_slab_in_gb]}
-  capacities = partner_capacity
-  delivery_partners = []
-  todo_deliveries.each do |delivery|
-    theater_partners = get_partners_list_by_theater(delivery[:theater])
-    partners = theater_partners.select {|x| x[:size_slab_in_gb].include?(delivery[:size_slab_in_gb])}
-     if partners.empty?
-      puts "#{delivery[:delivery_id]}, false, '', ''"
-      next
-    end
-    best_partner = find_best_partner(partners, delivery, capacities)
-    puts "#{delivery[:delivery_id]}, true, #{best_partner[:partner]}, #{best_partner[:best_partner_cost]}"
-    capacities[best_partner[:partner].to_sym] -= delivery[:size_slab_in_gb]
+output_2 = CSV.open("output2.csv", "wb")
+
+data = CSV.read("input.csv", { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all})
+todo_deliveries = data.map { |d| d.to_hash }
+todo_deliveries = todo_deliveries.sort {|x, y | y[:size_slab_in_gb] <=> x[:size_slab_in_gb]}
+capacities = partner_capacity
+delivery_partners = []
+todo_deliveries.each do |delivery|
+  theater_partners = get_partners_list_by_theater(delivery[:theater])
+  partners = theater_partners.select {|x| x[:size_slab_in_gb].include?(delivery[:size_slab_in_gb])}
+   if partners.empty?
+    output_2 << [delivery[:delivery_id], false, nil, nil]
+    next
   end
-  # puts "partner capacities left out: "
-  # puts capacities
+  best_partner = find_best_partner(partners, delivery, capacities)
+  output_2 << [delivery[:delivery_id], true, best_partner[:partner], best_partner[:best_partner_cost]]
+  capacities[best_partner[:partner].to_sym] -= delivery[:size_slab_in_gb]
 end
-
-find_delivery_partners
-
-
-# D4, false, '', ''
-# D3, true, P3, 15300
-# D2, true, P1, 3250
-# D1, true, P2, 3000
